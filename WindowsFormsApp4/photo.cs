fa1f026930ecf770af3633de495c2e22b821fadd
Xu3is <mleliukov@yandex.ru>
2025-05-16 11:45:56 +0300

    added classes

---
+++ WindowsFormsApp4/photo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace WindowsFormsApp4
+{
+    public class Photo
+    {
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTaken { get; set; }
+        public Photo(string path, string description, DateTime dateTaken)
+        {
+            Path = path;
+            Description = description;
+            DateTaken = dateTaken;
+        }
+        public override string ToString()
+        {
+            return $"{Path} - {Description} ({DateTaken.ToString("dd.MM.yyyy")})";
+        }
+    }
+}
